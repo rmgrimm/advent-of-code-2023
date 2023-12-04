e30dc7c29e861fbad627563fbe298cf3a3efa194
Robert Grimm <rmgrimm@users.noreply.github.com>
2023-12-04 00:59:07 -0600

    Complete day 4

--- day04/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day04/Day04Challenge1.kt
+++ day04/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day04/Day04Challenge1.kt
@@ -1,13 +1,45 @@
 package com.github.rmgrimm.adventofcode2023.day04
 
 import com.github.rmgrimm.adventofcode2023.support.readResourceLines
+import kotlin.math.pow
+
+data class Scratchcard(
+    val id: Int,
+    val winningNumbers: Set<Int>,
+    val containedNumbers: Set<Int>,
+) {
+    val matchingNumbers = winningNumbers.intersect(containedNumbers)
+    val score = if (matchingNumbers.isEmpty()) {
+        0
+    } else {
+        2.0.pow(matchingNumbers.count() - 1).toInt()
+    }
+}
+
+fun parseCard(line: String) = Scratchcard(
+    id = line.substring("Card ".length..<line.indexOf(':'))
+        .trim().toInt(),
+
+    winningNumbers = line.substringAfter(':')
+        .substringBefore('|')
+        .splitToSequence(' ')
+        .filterNot(String::isNullOrBlank)
+        .map(String::toInt)
+        .toSet(),
+
+    containedNumbers = line.substringAfter('|')
+        .splitToSequence(' ')
+        .filterNot(String::isNullOrBlank)
+        .map(String::toInt)
+        .toSet()
+)
 
 fun main() {
     println(
         readResourceLines("input")
-            .ifEmpty { sequenceOf("TODO") }
-            .first()
+            .map(::parseCard)
+            .sumOf(Scratchcard::score)
     )
 
-    // The correct answer is: ???
+    // The correct answer is: 17782
 }
--- day04/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day04/Day04Challenge2.kt
+++ day04/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day04/Day04Challenge2.kt
@@ -3,11 +3,23 @@ package com.github.rmgrimm.adventofcode2023.day04
 import com.github.rmgrimm.adventofcode2023.support.readResourceLines
 
 fun main() {
+    val cardList = readResourceLines("input")
+        .map(::parseCard)
+        .toList()
+
+    val cardCounts = IntArray(cardList.size) { 1 }
+
     println(
-        readResourceLines("input")
-            .ifEmpty { sequenceOf("TODO") }
-            .first()
+        cardList.sumOf { card ->
+            cardCounts[card.id - 1]
+                .also { cardCount ->
+                    (card.id..<(card.id + card.matchingNumbers.size))
+                        .asSequence()
+                        .filter { it <= cardCounts.lastIndex }
+                        .forEach { cardCounts[it] += cardCount }
+                }
+        }
     )
 
-    // The correct answer is: ???
+    // The correct answer is: 8477787
 }
