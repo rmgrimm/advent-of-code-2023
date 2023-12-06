845e6883322846a1c1023c0bd0a1d6b3371e6730
Robert Grimm <rmgrimm@users.noreply.github.com>
2023-12-05 23:38:10 -0600

    Day 6 challenges completed

--- day06/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day06/Day06Challenge1.kt
+++ day06/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day06/Day06Challenge1.kt
@@ -5,9 +5,24 @@ import com.github.rmgrimm.adventofcode2023.support.readResourceLines
 fun main() {
     println(
         readResourceLines("input")
-            .ifEmpty { sequenceOf("TODO") }
-            .first()
+            .map { line ->
+                line.substringAfter(':')
+                    .splitToSequence(' ')
+                    .filter(String::isNotBlank)
+                    .map(String::toInt)
+            }
+            .zipWithNext()
+            .flatMap { (timeSeq, distSeq) -> timeSeq zip distSeq }
+            .map { (maxTime, thresholdDistance) ->
+                (1..<maxTime).asSequence()
+                    .map { time ->
+                        time to (maxTime - time) * time
+                    }
+                    .filter { (_, distance) -> distance > thresholdDistance }
+                    .count()
+            }
+            .reduce(Int::times)
     )
 
-    // The correct answer is: ???
+    // The correct answer is: 293046
 }
--- day06/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day06/Day06Challenge2.kt
+++ day06/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day06/Day06Challenge2.kt
@@ -5,9 +5,21 @@ import com.github.rmgrimm.adventofcode2023.support.readResourceLines
 fun main() {
     println(
         readResourceLines("input")
-            .ifEmpty { sequenceOf("TODO") }
-            .first()
+            .map { line ->
+                line.replace(" ", "")
+                    .substringAfter(':')
+                    .toLong()
+            }
+            .zipWithNext()
+            .map { (maxTime, thresholdDistance) ->
+                (1..maxTime).asSequence()
+                    .map { time ->
+                        (maxTime - time) * time
+                    }
+                    .filter { distance -> distance > thresholdDistance }
+                    .count()
+            }
     )
 
-    // The correct answer is: ???
+    // The correct answer is: 35150181
 }
