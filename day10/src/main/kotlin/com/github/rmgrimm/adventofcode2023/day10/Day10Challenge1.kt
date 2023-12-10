31f2f6fa41b8cf2dc24948a358438c62c3ebdafb
Robert Grimm <rmgrimm@users.noreply.github.com>
2023-12-09 23:56:50 -0600

    Complete day 10

--- day10/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day10/Day10Challenge1.kt
+++ day10/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day10/Day10Challenge1.kt
@@ -2,12 +2,103 @@ package com.github.rmgrimm.adventofcode2023.day10
 
 import com.github.rmgrimm.adventofcode2023.support.readResourceLines
 
-fun main() {
-    println(
-        readResourceLines("input")
-            .ifEmpty { sequenceOf("TODO") }
-            .first()
-    )
+enum class Direction(val xDelta: Int, val yDelta: Int) {
+    NORTH(0, -1),
+    EAST(1, 0),
+    SOUTH(0, 1),
+    WEST(-1, 0);
 
-    // The correct answer is: ???
+    fun moveFrom(pos: Pair<Int, Int>) =
+        (pos.first + xDelta) to (pos.second + yDelta)
+}
+
+val opposites = mapOf(
+    Direction.NORTH to Direction.SOUTH,
+    Direction.EAST to Direction.WEST,
+    Direction.SOUTH to Direction.NORTH,
+    Direction.WEST to Direction.EAST
+)
+
+enum class PipeType(val symbol: Char, val directions: Set<Direction>) {
+    VERTICAL('|', setOf(Direction.NORTH, Direction.SOUTH)),
+    HORIZONTAL('-', setOf(Direction.WEST, Direction.EAST)),
+    NORTH_AND_EAST('L', setOf(Direction.NORTH, Direction.EAST)),
+    NORTH_AND_WEST('J', setOf(Direction.NORTH, Direction.WEST)),
+    SOUTH_AND_WEST('7', setOf(Direction.WEST, Direction.SOUTH)),
+    SOUTH_AND_EAST('F', setOf(Direction.EAST, Direction.SOUTH)),
+    GROUND('.', emptySet()),
+    START('S', emptySet())
+}
+
+val pipeTypeBySymbol = PipeType.entries.associateBy { it.symbol }
+
+data class ParsedMap(
+    val startPos: Pair<Int, Int>,
+    val map: Array<Array<PipeType>>
+) {
+    val startDirection = sequenceOf(
+        Direction.NORTH,
+        Direction.EAST,
+        Direction.SOUTH,
+        Direction.WEST
+    )
+        .first { direction ->
+            opposites[direction] in pipeAt(
+                direction.moveFrom(
+                    startPos
+                )
+            ).directions
+        }
+
+    fun pipeAt(pos: Pair<Int, Int>) =
+        map[pos.second][pos.first]
+}
+
+fun parseMap(inputName: String): ParsedMap {
+    val lines = readResourceLines(inputName)
+        .toList()
+
+    var startPos: Pair<Int, Int> = 0 to 0
+
+    val output = Array(size = lines.size) { y ->
+        val line = lines[y]
+        Array(size = line.length) { x ->
+            (pipeTypeBySymbol[line[x]] ?: PipeType.GROUND)
+                .also {
+                    if (it == PipeType.START) {
+                        startPos = x to y
+                    }
+                }
+        }
+    }
+
+    return ParsedMap(
+        startPos = startPos,
+        map = output
+    )
+}
+
+fun main() {
+    val parsedMap = parseMap("input")
+
+    var currentPos = parsedMap.startPos
+    var nextDirection = parsedMap.startDirection
+    var moveCount = 0
+
+    do {
+        currentPos = nextDirection.moveFrom(currentPos)
+        val pipe = parsedMap.pipeAt(currentPos)
+        val movedFromDirection = opposites[nextDirection]
+
+        for (possibleDirection in pipe.directions) {
+            if (possibleDirection != movedFromDirection) {
+                nextDirection = possibleDirection
+            }
+        }
+        moveCount++
+    } while (currentPos != parsedMap.startPos)
+
+    println(moveCount / 2)
+
+    // The correct answer is: 6717
 }
--- day10/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day10/Day10Challenge2.kt
+++ day10/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day10/Day10Challenge2.kt
@@ -1,13 +1,47 @@
 package com.github.rmgrimm.adventofcode2023.day10
 
-import com.github.rmgrimm.adventofcode2023.support.readResourceLines
+import java.awt.geom.Path2D
 
 fun main() {
+    val parsedMap = parseMap("input")
+
+    var currentPos = parsedMap.startPos
+    var nextDirection = parsedMap.startDirection
+
+    val linePositions: MutableSet<Pair<Int, Int>> = mutableSetOf()
+
+    val shape = Path2D.Double()
+    shape.moveTo(currentPos.first.toDouble(), currentPos.second.toDouble())
+
+    do {
+        linePositions += currentPos
+        shape.lineTo(currentPos.first.toDouble(), currentPos.second.toDouble())
+
+        currentPos = nextDirection.moveFrom(currentPos)
+        val pipe = parsedMap.pipeAt(currentPos)
+        val movedFromDirection = opposites[nextDirection]
+
+        for (possibleDirection in pipe.directions) {
+            if (possibleDirection != movedFromDirection) {
+                nextDirection = possibleDirection
+            }
+        }
+    } while (currentPos != parsedMap.startPos)
+
+    shape.closePath()
+
     println(
-        readResourceLines("input")
-            .ifEmpty { sequenceOf("TODO") }
-            .first()
+        parsedMap.map
+            .asSequence()
+            .flatMapIndexed { y, xArray ->
+                xArray.asSequence()
+                    .withIndex()
+                    .filter { (x, _) ->
+                        !linePositions.contains(x to y) &&
+                            shape.contains(x.toDouble(), y.toDouble())
+                    }
+            }.count()
     )
 
-    // The correct answer is: ???
+    // The correct answer is: 381
 }
