eb5fb6cf6f0bb74d3e0cda4fef27c01e55499829
Robert Grimm <rmgrimm@users.noreply.github.com>
2023-12-13 12:15:05 -0600

    Day 13 challenge 1 complete

--- day13/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day13/Day13Challenge1.kt
+++ day13/src/main/kotlin/com/github/rmgrimm/adventofcode2023/day13/Day13Challenge1.kt
@@ -1,13 +1,57 @@
 package com.github.rmgrimm.adventofcode2023.day13
 
-import com.github.rmgrimm.adventofcode2023.support.readResourceLines
+import com.github.rmgrimm.adventofcode2023.support.readGroupedPuzzleInputs
+
+fun parseBitMap(input: List<String>) =
+    LongArray(input.size) { index ->
+        input[index].replace('#', '1')
+            .replace('.', '0')
+            .toLong(2)
+    }
+
+fun parseTransposedBitMap(input: List<String>) =
+    LongArray(input.firstOrNull()?.length ?: 0)
+        .also { outputs ->
+            for (line in input) {
+                for ((index, char) in line.withIndex()) {
+                    outputs[index] = (outputs[index] shl 1) +
+                        if (char == '#') {
+                            1
+                        } else {
+                            0
+                        }
+                }
+            }
+        }
+
+fun findMirrorPoint(puzzle: LongArray) =
+    puzzle.asSequence()
+        .zipWithNext()
+        .mapIndexedNotNull { index, (left, right) ->
+            if (left == right) {
+                index + 1
+            } else {
+                null
+            }
+        }
+        .filter { possibleMirrorIndex ->
+            (possibleMirrorIndex - 1 downTo 0).map(puzzle::get)
+                .zip((possibleMirrorIndex..puzzle.lastIndex).map(puzzle::get))
+                .all { (left, right) -> left == right }
+        }
+        .firstOrNull() ?: 0
 
 fun main() {
     println(
-        readResourceLines("input")
-            .ifEmpty { sequenceOf("TODO") }
-            .first()
+        readGroupedPuzzleInputs("input")
+            .sumOf { puzzle ->
+                val vertMirrorValue = 100L * findMirrorPoint(parseBitMap(puzzle))
+                val horzMirrorValue =
+                    findMirrorPoint(parseTransposedBitMap(puzzle))
+
+                horzMirrorValue + vertMirrorValue
+            }
     )
 
-    // The correct answer is: ???
+    // The correct answer is: 35360
 }
--- support/src/main/kotlin/com/github/rmgrimm/adventofcode2023/support/ResourceFunctions.kt
+++ support/src/main/kotlin/com/github/rmgrimm/adventofcode2023/support/ResourceFunctions.kt
@@ -8,8 +8,35 @@ fun readResource(name: String): URL? =
 
 fun readResourceLines(
     name: String,
-    charset: Charset = Charsets.UTF_8) =
+    charset: Charset = Charsets.UTF_8
+) =
     readResource(name)
         ?.readText(charset)
         ?.lineSequence()
         ?.filter(String::isNotBlank)!!
+
+fun readGroupedPuzzleInputs(
+    name: String,
+    charset: Charset = Charsets.UTF_8
+) = sequence<List<String>> {
+    var currentPuzzle = mutableListOf<String>()
+
+    for (line in readResource(name)
+        ?.readText(charset)
+        ?.lineSequence() ?: emptySequence()) {
+
+        if (line.isBlank()) {
+            if (currentPuzzle.isNotEmpty()) {
+                yield(currentPuzzle)
+                currentPuzzle = mutableListOf()
+            }
+            continue
+        }
+
+        currentPuzzle.add(line)
+    }
+
+    if (currentPuzzle.isNotEmpty()) {
+        yield(currentPuzzle)
+    }
+}
